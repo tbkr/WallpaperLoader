c4e825448a224193ff6e0ed83a09c60d1ad00c94
Alexej Disterhoft <alexej@disterhoft.de>
2014-12-29 16:31:42 +0100

    Fixed argument forwarding

--- startLoader.sh
+++ startLoader.sh
@@ -1,1 +1,1 @@
-java -cp bin:lib/argparse4j-0.4.4.jar:lib/jsoup-1.8.1.jar -Dapple.awt.UIElement=true Main $@
+java -cp bin:lib/argparse4j-0.4.4.jar:lib/jsoup-1.8.1.jar -Dapple.awt.UIElement=true Main "$@"
